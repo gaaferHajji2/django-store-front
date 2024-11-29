2480f5130fdd69fc69b6d27334e9407da64f087c
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-29 22:57:50 +0300

    Adding APP To Installed APPS And Write First Request Handler Function - View Function

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # ! This Line Added By Jafar Loka
+    'playground'
 ]
 
 MIDDLEWARE = [
