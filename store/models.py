37b81f37cbc103ec019388278f4afdfdbbd4e6cd
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-01 00:05:50 +0300

    Adding Models To Project With Realtionship And Modifying Data

--- store/models.py
+++ store/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255, unique=True);
+
 class Product(models.Model):
     title = models.CharField(max_length=255, unique=True);
     description = models.TextField();
     price = models.DecimalField(max_digits=6, decimal_places=2);
     inventory = models.IntegerField();
     last_update = models.DateTimeField(auto_now=True);
+
+    collection = models.ForeignKey(to=Collection, on_delete=models.PROTECT);
 
 class Customer(models.Model):
 
@@ -43,3 +48,25 @@ class Order(models.Model):
 
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING);
 
+    customer = models.ForeignKey(to=Customer, on_delete=models.PROTECT);
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to=Order, on_delete=models.PROTECT);
+    product = models.ForeignKey(to=Product, on_delete=models.PROTECT);
+    quantity = models.PositiveSmallIntegerField();
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255);
+    city   = models.CharField(max_length=255);
+
+    customer = models.OneToOneField(to=Customer, on_delete=models.CASCADE, primary_key=True);
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True);
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE);
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE);
+
+    quantity = models.PositiveSmallIntegerField();
