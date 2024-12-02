0c995017329af3eda32f91ad9c1f35f3510c14b9
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-02 21:32:06 +0300

    Craete Generic Relationship

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.contenttypes.models import ContentType;
+
+from django.contrib.contenttypes.fields import GenericForeignKey;
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255);
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE);
+    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE);
+
+    object_id = models.PositiveIntegerField();
