46c91631a015c8ae6181fd92f1a8bdb15021f95c
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-06 14:30:53 +0300

    Apply Changes To Models And Migrate

--- store/models.py
+++ store/models.py
@@ -16,8 +16,11 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255, unique=True);
+
+    slug  = models.SlugField();
+
     description = models.TextField();
-    price = models.DecimalField(max_digits=6, decimal_places=2);
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2);
     inventory = models.IntegerField();
     last_update = models.DateTimeField(auto_now=True);
 
