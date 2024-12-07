2f4df069fb56d9e779e0ea239c4d3b6aef1ef756
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-07 13:07:24 +0300

    Redo The Changes

--- store/models.py
+++ store/models.py
@@ -50,12 +50,6 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True);
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE);
 
-    class Meta:
-        db_table = 'store_customers'; # Here we Define The Name Of Table.
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
-
 class Order(models.Model):
     PAYMENT_STATUS_PENDING  = 'P';
     PAYMENT_STATUS_COMPLETE = 'C';
