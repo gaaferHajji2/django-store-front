929d447dd2ac5270d9b316c4a58099d0f65b5aca
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-13 10:32:02 +0300

    Trying Query Set With Django ORM

--- playground/views.py
+++ playground/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from store.models import Product
+
 # Create your views here.
 def say_hello(request):
     # return HttpResponse('Hello World');
+
+    query_set = Product.objects.all();
+
+    for product in query_set:
+        print("The Product Is: ", product);
+
     return render(request, 'hello.html', { 'name': 'Jafar Loka'});
     # return render(request, 'hello.html');
