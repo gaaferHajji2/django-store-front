a0fd5341f9c9e34e010be395dcdaea7e0ac14adf
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-14 11:38:50 +0300

    Try Query Data With Filter

--- playground/urls.py
+++ playground/urls.py
@@ -5,5 +5,10 @@ from . import views;
 #URLConf
 urlpatterns = [
     path('hello/', views.say_hello),
-    path('hello-02/', views.say_hello_2)
+    path('hello-02/', views.say_hello_2),
+    path('hello-03/', views.say_hello_3),
+    path('hello-04/', views.say_hello_4),
+    path('hello-05/', views.say_hello_5),
+    path('hello-06/', views.say_hello_6),
+    path('hello-07/', views.say_hello_7),
 ];
--- playground/views.py
+++ playground/views.py
@@ -39,10 +39,76 @@ def say_hello_2(request):
 
     exists = Product.objects.filter(pk=0).exists();
 
+
+    print("\t\t", "-"*25);
+
     if product is None:
         print("No Product Found");
     
     print("The Product Data Is: ", product.description);
     print("The Check Of Product Exists is: ", exists);
 
-    return render(request, 'hello.html', { 'name': 'Jafar Loka-02'});
+    product_query_set = Product.objects.filter(unit_price__gt=20);
+    product_query_set_02 = Product.objects.filter(unit_price__lte=20);
+
+    # print("The Data Of Product Query Set Is: ", product_query_set);
+    # print("The Data Of Product Query Set Is: ", product_query_set_02);
+
+    product_query_set_03 = Product.objects.filter(unit_price__range=(20, 30));
+
+    print("The Data Of Product Query Set Range Is: ", product_query_set_03);
+
+    print("\t\t", "-"*25);
+
+
+    return render(request, 'hello.html', 
+        { 'name': 'Jafar Loka-02', 'products': list(product_query_set_03)});
+
+def say_hello_3(request):
+    # product_query_set_with_collection = Product.objects.filter(collection_id=2).query;
+    product_query_set_with_collection = Product.objects.filter(collection__id=6);
+    # product_query_set_with_collection = Product.objects.filter(collection__id__range=(4, 6));
+
+    # print("The Count Of Data Is: ", product_query_set_with_collection)
+
+    return render(request, 'hello.html', 
+        {   'name': 'Jafar Loka Test Relation', 
+            'products': list(product_query_set_with_collection)
+        }
+    )
+
+def say_hello_4(request):
+    product_query_set_with_collection = Product.objects.filter(title__contains='fruit');
+
+    return render(request, 'hello.html', 
+        {   'name': 'Jafar Loka Test Contains', 
+            'products': list(product_query_set_with_collection)
+        }
+    )
+
+def say_hello_5(request):
+    product_query_set_with_collection = Product.objects.filter(title__icontains='coffee');
+
+    return render(request, 'hello.html', 
+        {   'name': 'Jafar Loka Test iContains', 
+            'products': list(product_query_set_with_collection)
+        }
+    )
+
+def say_hello_6(request):
+    product_query_set_with_collection = Product.objects.filter(last_update__year=2021);
+
+    return render(request, 'hello.html', 
+        {   'name': 'Jafar Loka Test last update With Year', 
+            'products': list(product_query_set_with_collection)
+        }
+    )
+
+def say_hello_7(request):
+    product_query_set_with_collection = Product.objects.filter(description__isnull=True);
+
+    return render(request, 'hello.html', 
+        {   'name': 'Jafar Loka Test last update With Year', 
+            'products': list(product_query_set_with_collection)
+        }
+    )
