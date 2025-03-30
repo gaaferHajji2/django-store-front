e2ca4be74c0da634e66a64786ef36ac6058a7ba1
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-03-30 15:36:07 +0300

    Refactor Code

--- likes/models.py
+++ likes/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-from django.contrib.auth.models import User;
+from django.contrib.auth.models import User
 
 from django.contrib.contenttypes.models import ContentType
 
-from django.contrib.contenttypes.fields import GenericForeignKey;
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class LikedItem(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE);
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
 
-    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE);
+    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE)
 
-    object_id = models.PositiveIntegerField();
+    object_id = models.PositiveIntegerField()
 
-    content_object = GenericForeignKey();
+    content_object = GenericForeignKey()
