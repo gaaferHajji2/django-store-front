81e9c1080533f55e8b3c4140ea950b6bd4a93fe9
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-26 15:54:02 +0300

    Change Database For Building APIs

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,12 +85,23 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'storefront',
+    #     'HOST': 'localhost',
+    #     'USER': 'root',
+    #     'PASSWORD': '123',
+    # }
+
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront2',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': '123',
