3f64bf381e96461098c3407be5c833857f4e59e1
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-05-03 14:16:19 +0300

    Using Request And Response With api_view From Rest Framework

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
+    path('product/', views.product_list),
 ];
--- store/views.py
+++ store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view()
 def product_list(request):
-    return HttpResponse('Ok')
+    return Response('Ok')
