be3a30f67f79e5e3c2f29be1df04eaf90b913b65
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-05-04 12:59:40 +0300

    Adding HyperLink With Modifications For Including URLs For Collection Objects

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('product/', views.product_list),
     path('product/<int:id>/', views.product_detail),
-    path('collection/<int:id>/', views.collection_detail)
+    path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
 ];
