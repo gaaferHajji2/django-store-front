9b73c5d55f83eb25ffe988f2ebc18e2a5fea8379
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-05-06 20:31:01 +0300

    Fix A Code For Admin Site About Collection And Product

--- store/admin.py
+++ store/admin.py
@@ -101,7 +101,7 @@ class CollectionAdmin(admin.ModelAdmin):
     # In This Way We Can Override The Default Query Set
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            products_count=Count('product')
+            products_count=Count('products')
         )
 
 @admin.register(models.Customer)
