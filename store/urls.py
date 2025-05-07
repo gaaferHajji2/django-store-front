b31733c5bffc0edaa779b47a15736f2b866251fa
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-05-07 20:25:19 +0300

    Using Class Views For Product List

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/', views.product_list),
+    path('product/', views.ProductList.as_view()),
     path('product/<int:id>/', views.product_detail),
 
     # path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
--- store/views.py
+++ store/views.py
@@ -6,15 +6,32 @@ from django.db.models.aggregates import Count
 
 # from django.http import HttpResponse
 
+from rest_framework import status
+
+from rest_framework.views import APIView
+
 from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 
-from rest_framework import status
 
 from .models import Product, Collection
 
 from .serializers import ProductSerializer, CollectionSerializer
+
+class ProductList(APIView):
+    def get(self, request):
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={ 'request': request })
+        
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 # Create your views here.
 @api_view(['GET', 'POST'])
