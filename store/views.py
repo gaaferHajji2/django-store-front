81a651c9aa8510140aeb930ca5f372899a037b08
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-05-30 20:07:05 +0300

    Define Custom ViewSet For Cart And Register Cart URLs

--- store/views.py
+++ store/views.py
@@ -8,7 +8,9 @@ from django.db.models.aggregates import Count
 
 from rest_framework import status
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import CreateModelMixin
+
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 # from rest_framework.views import APIView
 
@@ -26,11 +28,11 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-from .models import Product, Collection, OrderItem, Review
+from .models import Product, Collection, OrderItem, Review, Cart
 
 from .filters import ProductFilter
 
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
 
 from .pagination import DefaultPagination
 
@@ -149,6 +151,10 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return { 'product_id': self.kwargs['product_pk'] }
 
+class CartViewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    
 
 # class CollectionList(ListCreateAPIView):
 
