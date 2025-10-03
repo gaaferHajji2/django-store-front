b213cd36b6481662094eb0a06cd9d95306521c97
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-03 14:52:28 +0300

    Adding core user serializer for get only

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,4 @@
-from djoser.serializers import UserCreateSerializer as Base
+from djoser.serializers import UserCreateSerializer as Base, UserSerializer as Base02
 
 from rest_framework import serializers
 
@@ -6,3 +6,7 @@ class UserCreateSerializer(Base):
 
     class Meta(Base.Meta):
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+
+class UserSerializer(Base02):
+    class Meta(Base02.Meta):
+        fields = ['id', 'first_name', 'email', 'last_name', 'username']
--- storefront/settings.py
+++ storefront/settings.py
@@ -172,13 +172,14 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 DJOSER = {
     'TOKEN_MODEL': None,
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer'
     },
     # other settings
 }
