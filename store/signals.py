3b1685da2e867e3be03b647dfc381b45ad1a7887
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-18 03:09:44 +0300

    Using signals for ctreating new customer and user

---
+++ store/signals.py
@@ -0,0 +1,11 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+from .models import Customer
+
+# sender: who sent the event to create the new customer
+# in our case it is core.models.User
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_new_customer_for_registered_user(sender, **kwargs):
+    if kwargs['created']:
+        Customer.objects.create(user=kwargs['instance'])
